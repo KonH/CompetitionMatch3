f43173f8135d3c1f8d81053e4e73429dfcd0c7df
KonH <konh@yandex.ru>
2017-01-15 13:03:29 +0700

    Better AI variants selection;

--- Assets/Scripts/Interface/SlotStateAI.cs
+++ Assets/Scripts/Interface/SlotStateAI.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class SlotStateAI : MonoBehaviour {
 
+	class Variant {
+		public SlotPosition LeftPos;
+		public SlotPosition RightPos;
+
+		public int Damage;
+
+		public Variant(SlotPosition leftPos, SlotPosition rightPos, int damage) {
+			LeftPos = leftPos;
+			RightPos = rightPos;
+			Damage = damage;
+		}
+	}
+
 	public SlotStateController Controller;
 	public SlotStateView View;
 
@@ -20,17 +33,32 @@ public class SlotStateAI : MonoBehaviour {
 	}
 
 	void TryMakeAction() {
-		Debug.Log("Try");
 		var rootState = Holder.CurrentState;
+		var rootHp = rootState.NextPlayer().CurrentPlayer.HP;
+		var variants = new List<Variant>();
 		foreach( var leftPos in rootState.Slots.Keys ) {
 			foreach( var rightPos in rootState.Slots.Keys ) {
 				if( SlotLogics.CanSwap(rootState, leftPos, rightPos) ) {
-					Controller.ResetSelection();
-					Controller.OnClick(leftPos);
-					Controller.OnClick(rightPos);
-					break;
+					var newState = SlotLogics.MakeSwap(rootState, leftPos, rightPos);
+					var newHP = newState.CurrentPlayer.HP;
+					var newVariant = new Variant(leftPos, rightPos, rootHp - newHP);
+					variants.Add(newVariant);
 				}
 			}
-		} 
+		}
+		MakeBestVariant(variants);
+	}
+
+	void MakeBestVariant(List<Variant> variants) {
+		var bestVariant = variants[0];
+		for( int i = 0; i < variants.Count; i++ ) {
+			var curVariant = variants[i];
+			if( curVariant.Damage > bestVariant.Damage ) {
+				bestVariant = curVariant;
+			}
+		}
+		Controller.ResetSelection();
+		Controller.OnClick(bestVariant.LeftPos);
+		Controller.OnClick(bestVariant.RightPos);
 	}
 }
